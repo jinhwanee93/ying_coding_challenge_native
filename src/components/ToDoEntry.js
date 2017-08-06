6915d145acc3e64d2677c19d33cd146268e6301f
Alexandre Kim <alexandrekim@Alexandres-MacBook-Pro.local>
2017-08-05 22:18:34 -0700

    pushing up changes, contniued to implement todo feature, auth is still in process

--- src/components/ToDoEntry.js
+++ src/components/ToDoEntry.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import {
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 class ToDoEntry extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      
+    }
   } 
+
+  handleEdit(e) {
+
+  }
+
+  handleDelete() {
+
+  }
 
   render() {
     return(
       <View>
+        <Text>{this.props.entry}</Text>
+        <Text>{this.props.isItCompleted}</Text>
+        <TouchableOpacity onPress={(e) => this.handleEdit(e)}></TouchableOpacity>
+        <TouchableOpacity onPress={() => this.handleDelete()}></TouchableOpacity>
       </View>    
     )
   }
--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import { 
   View, 
   Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
   StyleSheet 
 } from 'react-native';
-import TodoEntry from '../components/ToDoEntry'
+import TodoEntry from '../components/ToDoEntry';
+import axios from 'axios';
 
 class ToDoList extends Component {
 
   constructor() {
     super()
     this.state = {
-      entry: ''
+      entry: '',
+      todos: []
     }
     this.handleEntryChange = this.handleEntryChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8082/api/getAllTasks')
+    .then(result => {
+      console.log('what is the result object? ', result)
+      // this.setState({
+      //   todos: result.data
+      // })
+    })
   }
 
   handleEntryChange(e) {
@@ -22,10 +38,28 @@ class ToDoList extends Component {
     })
   }
 
+   handleAdd() {
+     const body = {
+       entry: this.state.entry
+     }
+    axios.post('http://localhost:8082/api/addTask', body)
+    .then( result => {
+      this.setState({
+        todos: [...this.state.todos, result.data]
+      })
+    })
+  }
+
   render() {
     return(
       <View>
-
+        <TextInput placeholder="write your todos here" onChangeText={(e) => this.handleEntryChange()}></TextInput>
+        <TouchableOpacity onPress={() => this.handleAdd()}><Text>Add</Text></TouchableOpacity>
+        <ScrollView>
+          {this.state.todos.map(todoEntries => {
+            <TodoEntry key={todoEntries.id} entry={todoEntries.entry} isItCompleted={todoEntries.isCompleted} />
+          })}
+        </ScrollView>
       </View>
     )
   }
